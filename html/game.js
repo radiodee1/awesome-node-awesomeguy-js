671af87076cf3e94a4e8587adb7bcba922af7df8
David Liebman <radiodee1@gmail.com>
2021-07-26 08:03:17 -0400

    menu seems to work from game.js

--- html/game.js
+++ html/game.js
@@ -1,17 +1,25 @@
 //console.log('process log')
 
 window.api.receive("sound", (data) => {
-   console.log(`Received toggle sound ${data} from main process`);
+   //console.log(`Received toggle sound ${data} from main process`);
+   var obj = document.getElementById("sound_checkbox");
+   //console.log(obj.checked);
+   obj.checked = ! obj.checked;
+   preferences_sound = obj.checked;
 });
 
 window.api.receive("restart", (data) => {
-   console.log(`Received restart ${data} from main process`);
+   //console.log(`Received restart ${data} from main process`);
+   restart_game();
 });
 
 window.api.receive("level", (data) => {
-   console.log(`Received level ${data} from main process`);
+   //console.log(`Received level ${data} from main process`);
    level = + data;
    var obj = document.getElementById("level_select");
    obj.selectedIndex = level + 1;
    restart_game();
 });
+
+var obj = document.getElementById("my_canvas");
+obj.focus();
