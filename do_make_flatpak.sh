eb2610a82e8b8dbe10c1c958333924d415f0e235
David Liebman <radiodee1@gmail.com>
2022-01-30 13:15:17 -0500

    working better after flatpak-builder install, but not totaly

--- do_make_flatpak.sh
+++ do_make_flatpak.sh
@@ -1,4 +1,9 @@
+## this assumes that flatpak-builder-tools is installed on the folder next to awesome-node-awesomeguy-js. The flatpak-builder-tools should be cloned there.
+
 npm install --package-lock-only
 cd flatpak
 
 python3 ../../flatpak-builder-tools/node/flatpak-node-generator.py npm --xdg-layout ../package-lock.json
+
+
+
